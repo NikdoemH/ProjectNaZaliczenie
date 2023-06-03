2efcc4c9af14f9a981aea4e1cbb974e0789f6a09
Nikodem Heitzman <nheitzman@edu.cdv.pl>
2023-06-03 15:02:19 +0200

    3Push
    3Push

---
+++ Assets/Counter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+
+public class Counter : MonoBehaviour
+{
+    public int a = 0;
+    public int b = 0;
+    public TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = $"{a} : {b}";
+    }
+}
